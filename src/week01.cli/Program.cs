0fa110529b7435d233a94923cc620b1df5552f9e
andrlikjirka <andrlik.jirka@gmail.com>
2024-02-17 20:00:12 +0100

    solved week01 exercise

---
+++ src/week01.cli/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace week01.cli;
+
+internal class Week01
+{
+    public static void Main(string[] args)
+    {
+        // Saying Hello to names given as shell args
+        foreach (var arg in args)
+        {
+            Console.WriteLine("Hello, " + arg);   
+        }
+    }
+}
