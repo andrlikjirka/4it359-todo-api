48419bc2e575360d4d4a9f53496b42af23ba8663
andrlikjirka <andrlik.jirka@gmail.com>
2024-02-18 18:17:55 +0100

    rename class

--- src/week01.cli/Program.cs
+++ src/week01.cli/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace week01.cli;
 
-internal class Week01
+internal class Program
 {
     public static void Main(string[] args)
     {
-        // Saying Hello to names given as shell args
+        // Saying Hello to people with names given as shell args
         foreach (var arg in args)
         {
             Console.WriteLine("Hello, " + arg);   
